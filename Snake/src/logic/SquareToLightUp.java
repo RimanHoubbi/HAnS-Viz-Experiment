0d47fe1f5e09e15cd798c8e170bff2c09ddae77f
RimanHoubbi <95256096+RimanHoubbi@users.noreply.github.com>
2024-11-04 22:09:40 +0100

    Add snake game

---
+++ Snake/src/logic/SquareToLightUp.java
@@ -0,0 +1,9 @@
+package logic;
+
+public enum SquareToLightUp {
+    SNAKE, // &line[Tile::Snake]
+    FOOD, // &line[Food]
+    POISON,  // &line[Poison] 
+    MILD_POISON,  // &line[MildPoison] 
+    BACK_GROUND // &line[Blank]
+}
