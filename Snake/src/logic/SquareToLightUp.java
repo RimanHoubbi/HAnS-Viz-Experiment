bbf2e0ba81f6c9b5da78f219ea31d94b5690b3d1
RimanHoubbi <95256096+RimanHoubbi@users.noreply.github.com>
2024-11-11 14:06:07 +0100

    Delete Snake directory

--- Snake/src/logic/SquareToLightUp.java
+++
@@ -1,9 +0,0 @@
-package logic;
-
-public enum SquareToLightUp {
-    SNAKE, // &line[Tile::Snake]
-    FOOD, // &line[Food]
-    POISON,  // &line[Poison] 
-    MILD_POISON,  // &line[MildPoison] 
-    BACK_GROUND // &line[Blank]
-}
